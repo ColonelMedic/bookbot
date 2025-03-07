1e67a5913478458baf3de742523aad85cdb13bce
Dennis L. <dennisleitelt@gmail.com>
2025-03-07 14:56:07 -0600

    updating python message

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+# bookbot
+
+def get_book_text(path):
+    # Open the file and return its contents
+    with open(path) as f:
+        return f.read()
+
+def main():
+    # Call get_book_text with the path and print the contents
+    path = "books/frankenstein.txt"  # Relative path to the book
+    text = get_book_text(path)
+    print(text)
+
+# Call the main function
+main()
